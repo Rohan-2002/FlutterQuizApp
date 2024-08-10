b93b07a27e9138b35209824e55fcb025ff8aa0be
rohan-2002 <82705624+Rohan-2002@users.noreply.github.com>
2024-08-10 22:12:35 +0530

    quiz app files

---
+++ lib/quiz.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/result_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  //concept used here is render content conditionally 
+   String currentScreeState = 'start-screen';
+   List<String> selectedAnswers = [];
+
+  void changeScreenState(){
+    setState(() {
+      currentScreeState = 'Questions-screen';
+    });
+  }
+
+  void chooseAnswer(String answer){
+    selectedAnswers.add(answer);
+    if(selectedAnswers.length == questions.length){
+      setState(() {
+        selectedAnswers = [];
+        currentScreeState = 'result-screen';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget widgetState = StartScreen(changeScreenState);
+    if(currentScreeState == 'Questions-screen'){
+      widgetState = QuestionsScreen(onSelectAnswer: chooseAnswer,);
+    }
+
+    if(currentScreeState == 'result-screen'){
+      widgetState = const ResultScreen();
+    }
+    return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      
+      body:Container( 
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color.fromARGB(255, 78, 13, 151), 
+                    Color.fromARGB(255, 107, 15, 168), 
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight
+          )
+        ),
+        child: widgetState,
+      ),
+    ),
+  );
+  }
+}
